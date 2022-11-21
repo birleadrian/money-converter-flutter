f6bdef40d2527304483618d157ee13e7b49f7cab
Adrian Birle <birle.adi@gmail.com>
2022-11-21 19:22:21 +0200

    added very_god_analysis package, added linter rules, resolved warnings from dart analysis

--- lib/main.dart
+++ lib/main.dart
@@ -29,15 +29,15 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final String messageConvertFromEur = "Convert from EUR to RON";
-  final String messageConvertFromRon = "Convert from RON to EUR";
-  double sum = 0.0;
-  String messageConvertFrom = "Convert from RON to EUR";
+  final String messageConvertFromEur = 'Convert from EUR to RON';
+  final String messageConvertFromRon = 'Convert from RON to EUR';
+  double sum = 0;
+  String messageConvertFrom = 'Convert from RON to EUR';
   String? error;
   double? inputValue;
-  String currencyNo1 = "RON";
-  String currencyNo2 = "EUR";
-  final myController = TextEditingController();
+  String currencyNo1 = 'RON';
+  String currencyNo2 = 'EUR';
+  final TextEditingController myController = TextEditingController();
 
   @override
   void dispose() {
@@ -48,14 +48,14 @@ class _MyHomePageState extends State<MyHomePage> {
   void compute() {
     setState(() {
       if (myController.text.isNotEmpty) {
-        if (currencyNo1 == "RON") {
+        if (currencyNo1 == 'RON') {
           sum = double.parse(myController.text) / 4.9;
         } else {
           sum = double.parse(myController.text) * 5.0;
         }
       } else {
         sum = 0.0;
-        myController.text = "";
+        myController.text = '';
       }
     });
   }
@@ -65,11 +65,11 @@ class _MyHomePageState extends State<MyHomePage> {
       if (messageConvertFrom == messageConvertFromRon) {
         messageConvertFrom = messageConvertFromEur;
         currencyNo1 = currencyNo2;
-        currencyNo2 = "RON";
+        currencyNo2 = 'RON';
       } else {
         messageConvertFrom = messageConvertFromRon;
         currencyNo1 = currencyNo2;
-        currencyNo2 = "EUR";
+        currencyNo2 = 'EUR';
       }
       compute();
     });
@@ -83,47 +83,49 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Center(
         child: Padding(
-          padding: const EdgeInsets.all(8.0),
+          padding: const EdgeInsets.all(8),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: <Widget>[
               Image.network(
-                  "https://thumbor.forbes.com/thumbor/fit-in/900x510/https://www.forbes.com/advisor/wp-content/uploads/2021/06/getty_currency-exchange_062321.jpeg.jpg"),
+                'https://thumbor.forbes.com/thumbor/fit-in/900x510/https://www.forbes.com/advisor/wp-content/uploads/2021/06/getty_currency-exchange_062321.jpeg.jpg',
+              ),
               Text(
                 messageConvertFrom,
               ),
-              Column(children: <Widget>[
-                TextField(
-                  controller: myController,
-                  keyboardType:
-                      const TextInputType.numberWithOptions(decimal: true),
-                  onChanged: (String input) {
-                    setState(() => {
-                          if (input.isEmpty)
-                            {error = "Please enter a number", sum = 0}
-                          else
-                            {
-                              inputValue = double.tryParse(input),
-                              if (inputValue == null)
-                                {
-                                  sum = 0,
-                                  error =
-                                      "Incorrect format - Please enter only numbers"
-                                }
-                              else
-                                {error = null}
-                            }
-                        });
-                  },
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(50),
+              Column(
+                children: <Widget>[
+                  TextField(
+                    controller: myController,
+                    keyboardType:
+                        const TextInputType.numberWithOptions(decimal: true),
+                    onChanged: (String input) {
+                      setState(() {
+                        if (input.isEmpty) {
+                          error = 'Please enter a number';
+                          sum = 0;
+                        } else {
+                          inputValue = double.tryParse(input);
+                          if (inputValue == null) {
+                            sum = 0;
+                            error =
+                                'Incorrect format - Please enter only numbers';
+                          } else {
+                            error = null;
+                          }
+                        }
+                      });
+                    },
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(50),
+                      ),
+                      hintText: currencyNo1,
+                      errorText: error,
                     ),
-                    hintText: currencyNo1,
-                    errorText: error,
                   ),
-                ),
-              ]),
+                ],
+              ),
               Text(
                 '${sum.toStringAsFixed(2)} $currencyNo2',
                 style: Theme.of(context).textTheme.headline4,
